8cf00208e15ffb1339a94f778242ee7f302a12db
Lam Nguyen Tung <lamnt4@smartosc.com>
2025-05-15 09:50:27 +0700

    adsssdssss

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -42,6 +42,35 @@
 
 // app/products/[slug]/page.tsx
 
+// import Product from "@/models/Product";
+// import type { IProduct } from "@/models/Product";
+// import { Types } from "mongoose";
+// import { notFound } from "next/navigation";
+// import ProductDetail from "@/components/products/ProductDetail";
+
+// export const revalidate = 300;
+
+// export default async function ProductDetailPage(props: {
+//   params: { slug: string };
+// }) {
+//   const { params } = props;
+//   const { slug } = params;
+//   const productDoc = await Product.findById(slug).lean<IProduct>().exec();
+
+//   if (!productDoc) return notFound();
+
+//   const product = {
+//     id: (productDoc._id as Types.ObjectId).toString(),
+//     title: productDoc.title,
+//     description: productDoc.description,
+//     price: productDoc.price,
+//     compareAtPrice: productDoc.compareAtPrice,
+//     thumbnail: productDoc.thumbnail,
+//   };
+
+//   return <ProductDetail {...product} />;
+// }
+
 import Product from "@/models/Product";
 import type { IProduct } from "@/models/Product";
 import { Types } from "mongoose";
@@ -50,11 +79,15 @@ import ProductDetail from "@/components/products/ProductDetail";
 
 export const revalidate = 300;
 
-export default async function ProductDetailPage(props: {
-  params: { slug: string };
-}) {
-  const { params } = props;
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
   const { slug } = params;
+
   const productDoc = await Product.findById(slug).lean<IProduct>().exec();
 
   if (!productDoc) return notFound();
