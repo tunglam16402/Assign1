21045c5f5f764bdc5561fe5dc988d08bdb6c35c1
Lam Nguyen Tung <lamnt4@smartosc.com>
2025-05-19 09:33:53 +0700

    update cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,10 +1,15 @@
-import CartCheckOut from "@/components/cart/CartCheckOut";
+// app/cart/page.tsx
+import dynamic from "next/dynamic";
 import Breadcrumb from "@/components/common/Breadcrumb";
 
-export default async function CartCheckoutPage() {
+const CartCheckOut = dynamic(() => import("@/components/cart/CartCheckOut"), {
+  ssr: false,
+});
+
+export default function CartCheckoutPage() {
   return (
     <div className="w-main mx-auto">
-      <Breadcrumb base="cart" current=""/>
+      <Breadcrumb base="cart" current="" />
       <CartCheckOut />
     </div>
   );
