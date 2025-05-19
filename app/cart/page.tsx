9af70184d59b844bdd6f12fbb7604f235817a57d
Lam Nguyen Tung <lamnt4@smartosc.com>
2025-05-19 10:06:38 +0700

    change cart to client component

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,12 @@
+"use client";
 
 import CartCheckOut from "@/components/cart/CartCheckOut";
 import Breadcrumb from "@/components/common/Breadcrumb";
 
-export default async function CartCheckOutPage() {
+export default function CartCheckOutPage() {
   return (
     <div className="w-main mx-auto">
-      <Breadcrumb base="cart" current=""/>
+      <Breadcrumb base="cart" current="" />
       <CartCheckOut />
     </div>
   );
